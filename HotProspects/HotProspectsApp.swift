bb3ba0529002504e5784e625dbfc1b049fd6ab50
enesozmus <ensozmus@gmail.com>
2024-04-05 14:29:19 +0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by enesozmus on 5.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
